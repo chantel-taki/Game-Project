09d302c658b33f6156f97d3c49331e4310125b1f
Chantel Taki <chanteltaki@gmail.com>
2020-04-09 07:48:17 -0400

    added sounds

--- game/src/main.js
+++ game/src/main.js
@@ -8,7 +8,8 @@ let endImg;
 function preload (){
   soundFormats('mp3', 'ogg');
  mySound = loadSound('assets/happy.mp3')
- //coughSound = loadSound('assets/old-man-cough.flac')
+ coughSound = loadSound('assets/cough.mp3')
+ jumpSound = loadSound('assets/Jump.mp3')
  game.initialize();
  startImg = createImg("assets/StartScreen.gif").hide();
  endImg = createImg("assets/EndScreen.gif").hide();
@@ -19,6 +20,7 @@ function setup() {
   game.setup();
   mySound.play();
   mySound.loop();
+  mySound.setVolume(0.5);
 
   } 
   
@@ -33,8 +35,8 @@ function setup() {
       endImg.show();
       game.player.img.hide();
       startImg.hide();
-     // coughSound.play();
-     // coughSound.loop();
+      coughSound.play();
+      coughSound.loop();
      // game.background.images.splice(0, 4)
      // game.background.images.src.forEach(element => {
        // element.hide();
@@ -51,6 +53,7 @@ function setup() {
     // W key
     if (keyCode === 87) {
       game.player.jump();
+      jumpSound.play();
     // game.player.img = createImg("assets/Player Jumping.gif").show();
       clear();
     }
